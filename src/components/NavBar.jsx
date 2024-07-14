85eba49b2aa207f9c4ca6a9ff71e19cd0f162402
Lucasr042 <rodriguezsilvalucas@gmail.com>
2024-07-14 19:06:37 -0300

    finalentrega1

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => (
+
+    <><><h1>AlibaMarket</h1></><ul>
+        <li>
+            <a href="#">Tarjetas Gráficas</a>
+        </li>
+        <li>
+            <a href="#">Procesadores</a></li>
+        <li>
+            <a href="#">Ram</a>
+        </li>
+    </ul>
+    <CartWidget />
+    </>
+
+);
